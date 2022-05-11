84f179df91545f61ea15f771a7e8532e6570601b
Cristian Blanaru <cristian.blanaru@bunnyshell.com>
2022-05-11 20:39:01 +0200

    simple demo module

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.2.0"
+    }
+  }
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = false
+}
+
+output "db_key" {
+    value = random_string.random.result
+}
